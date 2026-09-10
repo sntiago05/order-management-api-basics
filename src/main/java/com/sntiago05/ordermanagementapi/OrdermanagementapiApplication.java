2c07f5b291a434ddd4c33ed5511c544943d2e6a6
sntiago05 <santiagoarm2004@gmail.com>
2026-09-09 20:15:11 -0500

    Initialize Spring Boot project with Maven configuration and basic setup

---
+++ src/main/java/com/sntiago05/ordermanagementapi/OrdermanagementapiApplication.java
@@ -0,0 +1,13 @@
+package com.sntiago05.ordermanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdermanagementapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdermanagementapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sntiago05/ordermanagementapi/OrdermanagementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sntiago05.ordermanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdermanagementapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
