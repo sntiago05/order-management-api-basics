1eda630e632c210a422fd89a22ee6803e7e69d71
sntiago05 <santiagoarm2004@gmail.com>
2026-09-11 17:43:55 -0500

    Add endpoint to update product stock

--- src/main/java/com/sntiago05/ordermanagementapi/controller/ProductController.java
+++ src/main/java/com/sntiago05/ordermanagementapi/controller/ProductController.java
@@ -1,4 +1,25 @@
 package com.sntiago05.ordermanagementapi.controller;
 
+import com.sntiago05.ordermanagementapi.dto.UpdateStockRequest;
+import com.sntiago05.ordermanagementapi.dto.UpdateStockResponse;
+import com.sntiago05.ordermanagementapi.services.ProductService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@RequiredArgsConstructor
+@Validated
 public class ProductController {
+    private final ProductService productService;
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<UpdateStockResponse> updateStock(@Valid @RequestBody UpdateStockRequest request, @Positive(message = "id must be greater than 0") @PathVariable long id) {
+        return ResponseEntity.ok(productService.updateStock(request, id));
+    }
+
 }
